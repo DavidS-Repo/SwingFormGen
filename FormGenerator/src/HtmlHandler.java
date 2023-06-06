fc8fb5e083b9b9525781f81a85d89599ee643b12
David Serrano <serranodavid983@gmail.com>
2023-06-06 12:53:28 -0700

    Add files via upload
    Add HtmlHandler class for HTML handling
    
    This commit adds a new class called `HtmlHandler` that encapsulates the functionality related to HTML handling in the FormGenerator application. The `HtmlHandler` class provides methods for generating HTML code based on form elements and saving HTML to a file.
    
    The following changes were made:
    - Created a new Java class called `HtmlHandler` in a separate file.
    - Implemented the `generateHTML()` method in the `HtmlHandler` class to generate HTML code based on form elements.
    - Implemented the `saveHTMLToFile()` method in the `HtmlHandler` class to save HTML code to a file.
    - Updated the `FormGenerator` class to use the `HtmlHandler` class for HTML handling.
    
    By introducing the `HtmlHandler` class, the codebase becomes more modular and organized. The new class abstracts the details of HTML generation and saving, improving code readability and maintainability.

---
+++ FormGenerator/src/HtmlHandler.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlHandler {
+	private List<JTextField> dynamicFields;
+	private List<JTextArea> staticHTMLAreas;
+
+	public HtmlHandler() {
+		dynamicFields = new ArrayList<>();
+		staticHTMLAreas = new ArrayList<>();
+	}
+
+	public String generateHTML(String title, JPanel elementsPanel) {
+		StringBuilder htmlBuilder = new StringBuilder();
+		htmlBuilder.append("<html><head><title>").append(title).append("</title></head><body>");
+		List<String> dynamicFieldValues = new ArrayList<>();
+		for (JTextField textField : dynamicFields) {
+			String dynamicField = textField.getText().trim();
+			if (!dynamicField.isEmpty()) {
+				dynamicFieldValues.add(dynamicField);
+			}
+		}
+		for (Component component : elementsPanel.getComponents()) {
+			if (component instanceof JPanel) {
+				JPanel panel = (JPanel) component;
+				Component[] panelComponents = panel.getComponents();
+				if (panelComponents.length > 1 && panelComponents[1] instanceof JTextField) {
+					JTextField textField = (JTextField) panelComponents[1];
+					String dynamicField = textField.getText().trim();
+					if (!dynamicField.isEmpty()) {
+						htmlBuilder.append(dynamicField);
+						dynamicFieldValues.remove(dynamicField);
+					}
+				} else if (panelComponents.length > 1 && panelComponents[1] instanceof JScrollPane) {
+					JScrollPane scrollPane = (JScrollPane) panelComponents[1];
+					JTextArea textArea = (JTextArea) scrollPane.getViewport().getView();
+					String staticHTML = textArea.getText().trim();
+					if (!staticHTML.isEmpty()) {
+						htmlBuilder.append(staticHTML);
+					}
+				}
+			}
+		}
+		for (String dynamicFieldValue : dynamicFieldValues) {
+			htmlBuilder.append(dynamicFieldValue);
+		}
+		htmlBuilder.append("</body></html>");
+		return htmlBuilder.toString();
+	}
+
+	public void saveHTMLToFile(String html, JFrame frame) {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Save HTML");
+		fileChooser.setFileFilter(new FileNameExtensionFilter("HTML Files", "html"));
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fileChooser.setSelectedFile(new File("template.html"));
+		int userSelection = fileChooser.showSaveDialog(frame);
+		if (userSelection == JFileChooser.APPROVE_OPTION) {
+			File fileToSave = fileChooser.getSelectedFile();
+			try (PrintWriter writer = new PrintWriter(fileToSave)) {
+				writer.println(html);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(frame, "Error saving HTML file: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	public void addDynamicField(JPanel panel, JTextField textField) {
+		dynamicFields.add(textField);
+	}
+
+	public void addStaticHTML(JTextArea textArea) {
+		staticHTMLAreas.add(textArea);
+	}
+}
